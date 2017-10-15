a39004ae45ee4f017b2383461da8e274a8740a1b
roshenac mitchell <roshenac@hotmail.com>
2017-10-15 15:55:21 +0100

    Initial Commit

---
+++ MazeGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MazeGenerator
+//
+//  Created by Roshenac Mitchell on 15/10/2017.
+//  Copyright © 2017 Roshenac Mitchell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
